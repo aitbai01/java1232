f50be40c962bb0053b867f0847f9f49759354e60
Aitbai <aytbaysaginbaev27@gmail.com>
2026-02-05 12:53:09 +0500

    3 ЗЖ дайын

---
+++ lab1/lab2.java
@@ -0,0 +1,12 @@
+public class lab2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Lab 2");
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+    }
+}
